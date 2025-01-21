feb49604ea221e6a5d8fcc2df61c42d0722faefe
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-01-21 16:39:49 +0100

    Update login.js

--- src/components/Auth/login.js
+++ src/components/Auth/login.js
@@ -10,7 +10,7 @@ const Login = () => {
 	const [isLoading, setIsLoading] = useState(true); // Pour gérer l'état de chargement de la page
 	const [loading, setLoading]     = useState(false); // Pour gérer l'état de chargement des données
 	const [appInfo, setAppInfo]     = useState(null); // État pour stocker les données
-    const [password, setPassword]   = useState('');
+        const [password, setPassword]   = useState('');
 	
 
 	const connexion = async (e)  => {
@@ -35,44 +35,21 @@ const Login = () => {
 		}
     };
 
-	const logout = () => {
-		setIsLoading(true);
-		setTimeout(() => {
-			if (window.Android && typeof window.Android.o00011 === 'function') {
-				window.Android.o00011(() => {
-				  localStorage.clear();
-				});
-			} else {
-				navigate('/un');
-			}
-		}, 1000);
-	};
 	
-
-    const handleSubmit = async (e) => {
-		e.preventDefault(); // Empêche le rechargement de la page lors de la soumission
-		setLoading(true);    // Active le chargement
-
-		try {
-			// Vérifier si l'interface Android est disponible
-			if (window.Android && typeof window.Android.s0075 === 'function') {
-				window.Android.s0075(password);
-				setPassword(''); // Réinitialise le champ de titre après la soumission
-			} else {
-				navigate('/un');
-				localStorage.clear();
-				setLoading(false);
-			}
-		} catch (error) {
-			// En cas d'erreur, affiche le message d'erreur
-			console.log(`Erreur : ${error.message}`);
-		} finally {
-			// Désactive le chargement après la soumission
-			setLoading(false);
+    const logout = () => {
+	setIsLoading(true);
+	setTimeout(() => {
+		if (window.Android && typeof window.Android.o00011 === 'function') {
+			window.Android.o00011(() => {
+			    localStorage.clear();
+			});
+		} else {
+			navigate('/un');
 		}
+	}, 1000);
     };
 
-
+	
     useEffect(() => {
         // Récupérer les données depuis localStorage dès que le composant est monté
         const data = localStorage.getItem('AppInfo');
