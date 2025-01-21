2f91caaeae532b40e6487af42211413701c83524
unknown <tcheuffa4.ja@gmail.com>
2025-01-21 10:44:26 +0100

    Nouvelle version du projet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 // App.js
 import React, { useEffect } from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as  Route, Routes  } from 'react-router-dom';
 import Launch  from './components/Config/Redirect';
-//import ProtectedRoute from './components/Config/ProtectedRoute';
-=======
-<<<<<<< HEAD
-import { BrowserRouter as Route, Routes  } from 'react-router-dom';
-import Launch  from './components/Config/Redirect';
-=======
-import { BrowserRouter as  Route, Routes  } from 'react-router-dom';
-import Launch  from './components/Config/Redirect';
-//import ProtectedRoute from './components/Config/ProtectedRoute';
->>>>>>> 3c9c7e7 (MAJ 13/01/2025)
->>>>>>> 6a1e78927fb5b735d45e6801231d4396d277dfeb
 import PageNotFound from "./components/Config/PageNotFound";
 import UnAuth from './components/Config/UnAuth';
 import Login    from './components/Auth/login';
@@ -30,15 +18,7 @@ const App = () => {
 	    if (!window.Android) {  
 		    localStorage.clear(); 
 			sessionStorage.clear(); 
-<<<<<<< HEAD
-			window.location.href = 'https://autre-nom-de-domaine';	
-=======
-<<<<<<< HEAD
 			window.location.href = 'https://fleetbo.com';	
-=======
-			window.location.href = 'https://autre-nom-de-domaine';	
->>>>>>> 3c9c7e7 (MAJ 13/01/2025)
->>>>>>> 6a1e78927fb5b735d45e6801231d4396d277dfeb
 		} 
 	}, []);
 
@@ -54,7 +34,7 @@ const App = () => {
 			<Route path="/un" element={<UnAuth />} />
 			
 			{/* Home */}
-            <Route path="/welcome" element={<Welcome />} />
+                        <Route path="/welcome" element={<Welcome />} />
 			
 			{/* Page non trouvée */}
 			<Route path="*" element={<PageNotFound />} />
