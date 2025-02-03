9d08d17d9734de09f28da575f8194f583acc8e9b
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-02-03 14:09:04 +0100

    Update Welcome.js

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -44,9 +44,7 @@ const Welcome = () => {
 
     return (
         <motion.div
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
+           transition={{ duration: 0.4 }}
         >
             <nav className="navbar">
                 <div className="navbar-left">
