2a5c15b28a4b41f0319f0124f51c298d341532e9
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-02-08 02:25:46 +0100

    Update Redirect.js

--- src/Components/Config/Redirect.js
+++ src/Components/Config/Redirect.js
@@ -1,1 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { motion } from "framer-motion";
+
+
+const Redirect = () => {
+
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+            setTimeout(() => {
+                const data = localStorage.getItem('AppInfo');
+                if (data) {
+                    const parsedData = JSON.parse(data);
+        
+                    if (parsedData.logged === true) {
+                        navigate('/welcome');
+                    } else {
+                        navigate('/login');
+                    }
+                }
+            }, 100); 
+    }, [navigate]);
+
+
+    return (
+        <motion.div transition={{ duration: 0.4 }} > </motion.div>
+    )
+};
+
+export default Redirect;
 
