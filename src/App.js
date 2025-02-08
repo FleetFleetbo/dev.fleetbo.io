b948f8423d9e503d26007ccc36c13ef7dc0e2ec7
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-02-08 02:52:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+
+import Redirect from "./Components/Config/Redirect";
+import Not from './Components/Config/Not';
+
 import Login from "./Components/Auth/Login";
 import Welcome from "./Components/Welcome";
+
 import './App.css';
 
 function App() {
+  useEffect(() => {	
+    if (!window.Android) {  
+        localStorage.clear(); 
+        sessionStorage.clear(); 
+        window.location.href = 'https://fleetbo.io';	
+    } 
+  }, []);
+
   return (
     <AnimatePresence mode="wait">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+
+          {/* Authentification */}
           <Route path="/login" element={<Login />} />
+          <Route path="/redirect" element={<Redirect />} />
+
           <Route path="/welcome" element={<Welcome />} />
+          
+          {/* Page non trouvée */}
+		    	<Route path="yo" element={<Not />} />
         </Routes>
       </Router>
     </AnimatePresence>  
