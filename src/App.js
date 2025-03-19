f10e2ef80863f981c356639fbb2ac8a673c79e11
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-19 01:51:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Redirect from "./Components/Config/Redirect";
+import Footer from "./Components/Config/Footer";
 import Not from './Components/Config/Not';
 
 import Login from "./Components/Auth/Login";
 import Welcome from "./Components/Welcome";
 
+import Tab1 from "./Components/Tabs/Tab1";
+import Tab2 from "./Components/Tabs/Tab2";
+import Tab3 from "./Components/Tabs/Tab3";
+
 import './App.css';
 
 function App() {
 
-  useEffect(() => {	
-    // WARNING! use localStorage only for test mode
-    localStorage.setItem('appId', 'your appId');
-    localStorage.setItem('database', 'your databaseName');
-    if (!window.fleetbo) {  
-        localStorage.clear(); 
-        sessionStorage.clear(); 
-        window.location.href = 'https://fleetbo.io';	
-    } 
-  }, []);	
+
+  useEffect(() => {
+
+    if (window.fleetbo) {
+      if(window.localStorage){
+           // WARNING! use localStorage only for test mode
+	    localStorage.setItem('appId', 'your appId');
+	    localStorage.setItem('database', 'your databaseName');
+      }
+    }else {
+      localStorage.clear(); 
+      sessionStorage.clear(); 
+      window.location.href = 'https://fleetbo.io';
+      return; // Arrête l'exécution si redirection
+    }
+  
+  }, []);
+  
+  
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
-    <AnimatePresence mode="wait">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
 
+          <Route path="/redirect" element={<Redirect isLoggedIn={isLoggedIn} />} />
           {/* Authentification */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/redirect" element={<Redirect />} />
+          <Route path="/login" element={<Login onLogin={() => setIsLoggedIn(true)} />} />
+          <Route path="/welcome" element={isLoggedIn ? <Welcome /> : <Navigate to="/login" />} />
 
-          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/tab1" element={isLoggedIn ? <Tab1 /> : <Navigate to="/login" />} />
+          <Route path="/tab2" element={isLoggedIn ? <Tab2 /> : <Navigate to="/login" />} />
+          <Route path="/tab3" element={isLoggedIn ? <Tab3 /> : <Navigate to="/login" />} />
+         
           
           {/* Page non trouvée */}
-	  <Route path="*" element={<Not />} />
+	        <Route path="*" element={<Not />} />
+          {/* Page non trouvée */}
+          <Route path="/footer" element={ <Footer /> } />
         </Routes>
       </Router>
-    </AnimatePresence>  
   );
 }
 
 export default App;
+
