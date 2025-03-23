b85c72427327a087ebd5540321d05a95aa5b456b
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-23 16:22:08 +0100

    Update Redirect.js

--- src/Components/Config/Redirect.js
+++ src/Components/Config/Redirect.js
@@ -1,32 +1,12 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { motion } from "framer-motion";
+import React from 'react';
 
+const Redirect = ()  => {
 
-const Redirect = () => {
-
-    const navigate = useNavigate();
-
-
-    useEffect(() => {
-            setTimeout(() => {
-                const data = localStorage.getItem('AppInfo');
-                if (data) {
-                    const parsedData = JSON.parse(data);
-        
-                    if (parsedData.logged === true) {
-                        navigate('/welcome');
-                    } else {
-                        navigate('/login');
-                    }
-                }
-            }, 100); 
-    }, [navigate]);
-
-
-    return (
-        <motion.div transition={{ duration: 0.4 }} > </motion.div>
-    )
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
 };
 
 export default Redirect;
