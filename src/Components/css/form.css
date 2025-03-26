2869ea5224f9b4b460e7b0018f68b0e51a896e0b
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-26 19:55:13 +0100

    Create form.css

---
+++ src/Components/css/form.css
@@ -0,0 +1,15 @@
+.div-form {
+    padding: 3%;
+    margin-top: 25%;
+  }
+  .input {
+   padding: 3%;
+   font-size: 18px;
+  }
+  
+  label {
+    font-weight: 500;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: #333;
+    font-size: 18px;
+  }
