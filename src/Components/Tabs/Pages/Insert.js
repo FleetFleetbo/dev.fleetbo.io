3bc90f4162ceba15b911e0fd484bb4516542e82c
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-26 18:45:54 +0100

    Create Insert.js

---
+++ src/Components/Tabs/Pages/Insert.js
@@ -0,0 +1,158 @@
+
+import React, { useState, useEffect } from 'react';
+import Fleetbo from '../helper/systemHelper';
+
+
+
+const Insert = () => {
+    
+    const [loading,  setLoading]  = useState();
+    const [loadpage, setLoadPage] = useState(true); 
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+        // No need to store current date. Automatic
+    });
+    const  fleetboDB                            = "vOWFCGQNcE2QyzlTMe8h";
+    const  db                                   = "items";
+    const [resultMessage, setResultMessage]     = useState();
+    const [messageType, setMessageType]         = useState(''); // 'success' or 'error'
+
+
+    useEffect(() => {
+        setTimeout(() => {  setLoadPage(false); }, 300);   
+    }, [loadpage]);
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    
+    const handleSubmit = (e) => {
+        setLoading(true);
+        e.preventDefault();
+        setResultMessage("");
+        setMessageType("");
+
+        if (!formData.title || !formData.content) {
+            setLoading(false);
+            setResultMessage('Champs obligatoires');
+            setMessageType('danger');
+            return;
+        }
+        
+        const jsonData   = JSON.stringify(formData);
+        // Insertion
+        setTimeout(() => {
+            Fleetbo.adn9991(fleetboDB, db, jsonData);
+            setFormData({ title: "", content: "" });
+            setLoading(false);
+        }, 1000);
+    };
+
+    window.onAddResult = (success) => {
+        if (success) {
+            setResultMessage('✅ Ajouté avec succès.');
+            setMessageType('success');
+            return;
+        } else {
+            setResultMessage("❌ Erreur lors de l'envoie");
+            setMessageType('danger');
+            return;
+        }
+    };
+    
+
+    return (
+        <>
+            <header className='navbar pt-4'> 
+                <div className=''> 
+                    <button onClick={() => Fleetbo.back() }  className="logout fs-5 fw-bold">
+                        <i className="fa-solid fa-arrow-left"></i> <span className='ms-3'>Insert</span>
+                    </button>
+                </div>
+                <div className="navbar-right">
+                    <button onClick={() => Fleetbo.openGalleryView() }  className="logout fw-bold">
+                         <i className="fa-solid fs-5 fa-image"></i>
+                    </button>
+                </div>
+            </header>
+
+            {/* Container avec gestion du loader */}
+            <div className="form-container" >
+            {loadpage ? (
+                <div className="loader"></div>
+            ) : (
+                <>
+                      <div className="form">
+                               
+                                <form onSubmit={handleSubmit} >
+                                  
+                                    <div className='mb-3'>
+                                       <div className='mt-1'>
+                                           <h3 className="float-start fw-bolder text-success" style={{ fontFamily: 'tahoma' }}>
+                                                Add item
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <br /> <br />
+  
+                                    <div className="mb-3">
+                                       <label className="float-start fs-5 mb-2">Title</label>
+                                       <input
+                                           type='text'
+                                           className="form-control input"
+                                           name="title"
+                                           value={formData.title}
+                                           onChange={handleChange}
+                                           
+                                       />
+                                    </div>
+       
+                                    <div className="mb-3">
+                                       <label className="float-start fs-5 mb-2">Description</label>
+                                       <textarea
+                                           type="text"
+                                           name="content"
+                                           value={formData.content}
+                                           onChange={handleChange}
+                                           className="form-control input"
+                                           rows={4}
+                                           
+                                       >
+                                       </textarea>
+                                    </div>
+       
+                                    <button
+                                        className="go mt-2 mb-2 fw-bold" 
+                                        onClick={handleSubmit} 
+                                        type="submit"
+                                        disabled={loading}
+                                    >
+                                        {loading ? 'Loading...' : 'Add'} 
+                                    </button>
+                                   <div className="text-success fw-normal mt-1" style={{ height: '20px'}}>
+                                        {/* Result Message */}
+                                        {resultMessage && (
+                                            <div className={`input-box mt-3 fs-6 text-${messageType === 'success' ? 'success' : 'danger'}`}>
+                                                {resultMessage}  
+                                            </div>
+                                        )}
+                                   </div>
+       
+                                </form>
+                       </div>
+                </>
+            )}
+            </div>
+        </>
+    );
+};
+
+export default Insert;
+
