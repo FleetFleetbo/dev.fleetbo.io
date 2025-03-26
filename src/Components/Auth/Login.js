72b7f0cbfe21a133046b7e02b6e0d934a40f22f7
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-26 18:52:24 +0100

    Update Login.js

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,45 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import Fleetbo from './../Tabs/helper/systemHelper';
 import { useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
 
 
 const Login = () => {
 
-    const navigate = useNavigate();
+    const navigate                  = useNavigate();
     const [isLoading, setIsLoading] = useState(true); // Pour gérer l'état de chargement de la page
     const [loading, setLoading]     = useState(false); // Pour gérer l'état de chargement des données
     const [appInfo, setAppInfo]     = useState(null); // État pour stocker les données``
 
-
-    const logout = async (e) => {
-        e.preventDefault(); 
-		setIsLoading(true);
-		setTimeout(() => {
-			if (typeof window.fleetbo.c00ey0 === 'function') {
-				window.fleetbo.c00ey0();
-			} 
-		}, 1000);
-	};
     
-     const handleSubmit = async (e) => {
-        e.preventDefault(); // Empêche le rechargement de la page lors de la soumission
-        setLoading(true);    // Active le chargement
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+        setLoading(true); 
     
         try {
-            if (typeof window.fleetbo.s0075 === 'function') {
-                window.fleetbo.s0075(); //  Appelle la fonction Android
-            } else {
-                localStorage.clear();
-                navigate('/un'); // Redirige si Android n'est pas disponible
-            }
+            Fleetbo.c0074(); 
         } catch (error) {
-            console.error(`Erreur : ${error.message}`); //  Meilleure gestion des erreurs
+            console.error(`Erreur : ${error.message}`); 
         } finally {
             setTimeout(() => setLoading(false), 1000); // Ajoute un petit délai pour un effet visuel fluide
         }
     };
+    
 
-     useEffect(() => {
+    useEffect(() => {
             // Récupérer les données depuis localStorage dès que le composant est monté
             const data = localStorage.getItem('AppInfo');
 
@@ -50,7 +37,7 @@ const Login = () => {
                  
                     // Vérifiez la valeur de 'logged' et mettez à jour l'état
                     if (parsedData.logged === true) {
-                        navigate('/welcome');
+                        navigate('/tab1');
                     } else {
                         setAppInfo(parsedData);
                     }
@@ -62,42 +49,44 @@ const Login = () => {
     }, [appInfo, navigate]);
 
 
-    if (isLoading) {
-        return <div className='parent-container'><><div className="loader">
-        </div></> </div>;
-    }
-
 
     return (
         <motion.div
             transition={{ duration: 0.4 }}
-            className="parent-container"
+            className="form-container"
         >
             <div className="container">
             {isLoading ? (
-                    <div className="parent-container">
-                        <div className="loader"></div>
-                    </div>
+                    <div className=""> </div>
                 ) : appInfo ? (
                     <>
-                        <img
-                            src={`${process.env.PUBLIC_URL}/logo.png`}
-                            className="img-login"
-                            alt="Logo"
-                        />
+ 
                         <div className="text-container">
-                            <h2>{appInfo.name}</h2>
-                            <div style={{ height: "70px" }}>
-                                <p>{appInfo.description}</p>
+                            {/*
+                            <div className='row'>
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/logo.png`}
+                                    className="img-login mt-2" 
+                                    alt="Logo"
+                                />
                             </div>
-                            <button onClick={handleSubmit} className="go">
-                                {loading ? "Chargement..." : "Se connecter"}
-                            </button>
+                            */}
+                            <div className='row mt-4'>
+                                <h2 className='fw-bolder'>{appInfo.name}</h2>
+                                <div style={{ height: "70px" }}>
+                                    <p style={{ textAlign: "left" }}>{appInfo.description}</p>
+                                </div>
+                                <div > 
+                                    <button onClick={handleSubmit} className="go">
+                                        {loading ? "Chargement..." : "Go"}
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                         <br />
                         <div className="pb-2">
-                            <button onClick={logout} className="btn-logout mt-3 text-secondary">
-                                <i className="fa-solid fa-power-off"></i> Quitter
+                            <button onClick={() => { setTimeout(() => { Fleetbo.d0a13() }, 500)  }} className="btn-logout mt-1 text-secondary">
+                                <i className="fa-solid fa-power-off"></i> Leave
                             </button>
                         </div>
                     </>
