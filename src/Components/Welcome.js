32c0754f3a0b41a611ede9d7c42dcabe89a3bb2f
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-03-28 12:34:27 +0100

    Update Welcome.js

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,91 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { motion } from "framer-motion";
+import Fleetbo from 'systemHelper';
 
 
-const Welcome = () => {
+const Welcome = ()  => {
 
-    const navigate                              = useNavigate();
-    const [isLoading, setIsLoading]             = useState(true); // Pour gérer l'état de chargement de la page
-    const [appInfo, setAppInfo]                 = useState({}); // État pour stocker les données
+  const [isNative, setIsNative] = useState(true);
 
+  useEffect(() => {
+    if(isNative){
+      Fleetbo.openView("Home", true);
+      setIsNative(true);
+    }
+      
+  },[isNative]);
 
-    const logout = () => {
-        
-	    setIsLoading(true);
-	    setTimeout(() => {
-			if (typeof window.fleetbo.d0a13 === 'function') {
-				window.fleetbo.d0a13();
-				localStorage.clear();
-			} else {
-				navigate('/un');
-			}
-	    }, 1000);
-    };
-
-
-   useEffect(() => {
-        setTimeout(() => {
-            try {
-                const data = localStorage.getItem("AppInfo");
-
-                if (data) {
-                    const parsedData = JSON.parse(data);
-
-                    if (!parsedData.logged) {
-                        navigate("/login");
-                    } else {
-                        setAppInfo(parsedData);
-                    }
-                } else {
-                    navigate("/un");
-                }
-            } catch (error) {
-                console.error("Erreur lors du parsing de AppInfo :", error);
-                localStorage.removeItem("AppInfo"); // Supprime les données corrompues
-            }
-            setIsLoading(false);
-        }, 1000);
-    }, [navigate]); // Supprime `appInfo` pour éviter des re-render inutiles
-
-
-   return (
-        <motion.div
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-        >
-            <nav className="navbar">
-                <div className="navbar-left">
-                    <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="Logo" className="logo" />
-                    {appInfo && appInfo.name && <h1 className="title">{appInfo.name}</h1>}
-                </div>
-                <div className="navbar-right">
-                    <button onClick={logout} className="logout">
-                        <i className="fa-solid fa-power-off"></i>
-                    </button>
-                </div>
-            </nav>
-
-            <div className="App-Container">
-                {isLoading ? (
-                    <div className="parent-container">
-                       <div className="loader"></div>
-                    </div>
-                ) : appInfo ? (
-                    <div className="text-container">
-                       <h2> Fleetbo </h2>
-                       <p>Welcome developer!</p>
-                    </div>
-                ) : (
-                    <div className="parent-container">
-                       <p>Impossible de charger les informations de l'app.</p>
-                    </div>
-                )}
-            </div>
-            
-        </motion.div>
-    )
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
 };
 
 export default Welcome;
