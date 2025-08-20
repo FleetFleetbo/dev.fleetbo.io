412aaebdfc478bb6d06e1b43aa2465b5fccdd159
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-08-20 17:21:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'; 
 import 'assets/css/Index.css'; 
 import App from './App'; 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { history } from 'components/layout/Navigation';
 
 const rootElement = document.getElementById('root');
 
 window.navigateToTab = (route) => {
-  window.history.pushState({}, "", route);
-  window.dispatchEvent(new PopStateEvent("popstate"));
+  history.push(route);
 };
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <HistoryRouter history={history}>
+      <App />
+    </HistoryRouter>
   </React.StrictMode>
 );
 
