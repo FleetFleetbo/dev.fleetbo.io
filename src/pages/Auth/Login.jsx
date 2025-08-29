066718684efa8ffbb7acd9e5e56c5fb967302e0c
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-08-29 15:27:50 +0200

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -2,28 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Fleetbo from 'api/fleetbo';
 import { useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
-import {  Link  } from 'react-router-dom';
+import logo from 'assets/images/logo.png';
 
 
 const Login = () => {
 
     const navigate                            = useNavigate();
     const [isLoading, setIsLoading]           = useState(true); 
-    const [loading, setLoading]               = useState(false); 
+    const [loadingLog, setLoadingLog]         = useState(false); 
     const [loadingLeave, setLoadingLeave]     = useState(false); 
     const [appInfo, setAppInfo]               = useState(null);
 
     
-    const handleSubmit = async (e) => {
-        e.preventDefault(); 
-        setLoading(true);   
-    
+    const log = async (e) => {
         try {
-            Fleetbo.c0074(); 
+            setLoadingLog(true)
+            Fleetbo.log(); 
         } catch (error) {
-            console.error(`Erreur : ${error.message}`);
+            console.log(`Erreur : ${error.message}`);
         } finally {
-            setTimeout(() => setLoading(false), 1000); 
+            setTimeout(() => setLoadingLog(false), 500); 
         }
     };
 
@@ -67,30 +65,27 @@ const Login = () => {
                         <></>
                         ) : appInfo ? (
                     <>
- 
                         <div className="text-container">
                             <div className='row mt-4 pb-2'>
-                                <h1 className='fw-bolder text-success fs-1'>Login </h1>
-                                
-                                <div className='pb-1' >
-                                    <h3 className='fw-bolder'>{appInfo.appName}</h3>
+                                <img
+                                    style={{ height: "95px", width:"110px"}}
+                                    src={logo}
+                                    alt="logo"
+                                />
+                                <div className='mt-3' style={{ height: "100%" }}>
+                                    <h3 className='fw-bolder' style={{ color: "#b37605" }}>{appInfo.appName}</h3>
                                     <p style={{ textAlign: "left" }}>{appInfo.description}</p>
                                 </div>
+                                
                                 <div className='mt-2' > 
-                                    <button onClick={handleSubmit} className="go mt-2">
-                                        {loading ? "Loading..." : "Go"}
+                                    <button onClick={log} className="go mt-2">
+                                        {loadingLog ? "Connexion..." : "Go"}
                                     </button>
                                 </div>
                             </div>
                         </div>
                         <br />
                         <div className="pb-1">
-                            <Link to="/register" className="btn-logout">
-                                New account
-                            </Link>
-                        </div>
-                        <br />
-                        <div className="pb-2">
                             <button onClick={leaveApp} className="btn-leave">
                                 <i className="fa-solid fa-power-off"></i> {loadingLeave ? "Leave..." : "Leave"}
                             </button>
@@ -98,7 +93,7 @@ const Login = () => {
                     </>
                 ) : (
                     <div>
-                        <p>Information not available</p>
+                        <p>Information not available 2</p>
                     </div>
                 )}
             </div>   
