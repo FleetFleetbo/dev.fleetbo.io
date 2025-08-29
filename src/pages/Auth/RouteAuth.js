d599c189e3b968638e006279d46c176add6e9b76
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-08-29 15:23:15 +0200

    Update RouteAuth.js

--- src/pages/Auth/RouteAuth.js
+++ src/pages/Auth/RouteAuth.js
@@ -4,7 +4,7 @@ import Loader from 'components/common/Loader';
 
 /**
  * Ce composant agit comme un aiguilleur.
- * Il lit l'état ('login' ou 'register') stocké par le code natif,
+ * Il lit l'état ('login' ou 'welcome') stocké par le code natif,
  * s'assure que la barre de navigation est masquée,
  * et redirige l'utilisateur vers la page appropriée en utilisant React Router.
  */
@@ -12,27 +12,32 @@ const RouteAuth = () => {
     const navigate = useNavigate(); 
 
     useEffect(() => {
-
+        // Fleetbo.setNavbarHide();
+        // On exécute la logique de redirection une seule fois au montage du composant.
         const data = localStorage.getItem('AppInfo');
+
         if (data) {
             try {
                 const parsedData = JSON.parse(data);
 
-                if (parsedData.state === "register") {
-                    navigate('/register', { replace: true });
+                // Logique de redirection basée sur l'état reçu.
+                if (parsedData.state === "welcome") {
+                    // Utiliser navigate pour rediriger
+                    navigate('/tab1', { replace: true });
                 } else {
+                    // Par défaut, on redirige vers 'login'
                     navigate('/login', { replace: true });
                 }
             } catch (error) {
                 console.error("Erreur de parsing pour AppInfo depuis localStorage", error);
+                // En cas d'erreur, on redirige vers login par sécurité.
                 navigate('/login', { replace: true });
             }
-        } else {
-            console.error("Aucune information AppInfo trouvée dans localStorage pour le routage.");
-            navigate('/login', { replace: true });
-        }
-    }, [navigate]);
+        } 
+    }, [navigate]); // Ajouter navigate aux dépendances de l'effet.
 
+    // On affiche un loader pendant que la logique de redirection s'exécute.
+    // L'utilisateur ne le verra qu'une fraction de seconde.
     return <Loader />;
 };
 
