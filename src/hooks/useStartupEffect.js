178ed8a6ad68deab0fb301f723a08378c86b5e5c
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-08-31 04:35:31 +0200

    Update useStartupEffect.js

--- src/hooks/useStartupEffect.js
+++ src/hooks/useStartupEffect.js
@@ -6,31 +6,26 @@ import { history } from 'components/layout/Navigation';
 export const useStartupEffect = () => {
         const location = useLocation(); 
 
-        // Cet effet s'exécute à chaque fois que l'URL change
         useEffect(() => {
-            // On attend un très court instant pour s'assurer que le DOM est bien à jour
             setTimeout(() => {
             console.log("React: Page rendue. Notification au natif.");
             Fleetbo.onWebPageReady();
-            }, 150); // Un petit délai de sécurité
-        }, [location]); // Se déclenche à chaque changement d'URL
+            }, 150); 
+        }, [location]); 
     
-        // Ce useEffect gère la redirection au démarrage
         useEffect(() => {
             const lastActiveTab = localStorage.getItem("activeTab") || "Tab1";
             const initialRoute = `/${lastActiveTab.toLowerCase()}`;
-    
-            if (location.pathname === '/' && initialRoute !== '/tab1') { // On agit seulement au démarrage
+            if (location.pathname === '/' && initialRoute !== '/tab1') { 
                 console.log(`Synchronisation de la route initiale vers : ${initialRoute}`);
                 history.push(initialRoute);
             }
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []); // Le tableau vide garantit une seule exécution au montage
+        }, []); 
 
         useEffect(() => {
           if (!window.fleetbo) {
             console.error("L'application doit être exécutée dans le conteneur natif.");
-            window.location.href = 'https://fleetbo.io/docs'; // Redirection agressive, à n'utiliser qu'en production
+            window.location.href = 'https://fleetbo.io/docs'; 
           }
         }, []);
 };
