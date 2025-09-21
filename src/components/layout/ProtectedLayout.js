416db0bd45960c15b7a0874344e62378a554c450
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-09-21 15:36:41 +0200

    Update ProtectedLayout.js

--- src/components/layout/ProtectedLayout.js
+++ src/components/layout/ProtectedLayout.js
@@ -7,7 +7,6 @@ const ProtectedLayout = () => {
     return (
         <div className="app-container">
             <main className="main-content">
-                {/* L'Outlet rendra la page (Tab1, Tab2...), RIEN DE PLUS */}
                 <Outlet />
             </main>
         </div>
