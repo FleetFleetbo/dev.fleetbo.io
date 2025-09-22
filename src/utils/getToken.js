859fd97c58c96a0abd66341b9cb1846ec4e02efd
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-09-22 13:27:24 +0200

    Update getToken.js

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -7,10 +7,8 @@ export const handleGetToken = async () => {
     try {
         // Use the Fleetbo API Promise
         const tokenResult = await Fleetbo.getToken();
-        console.log("FCM Token received:", tokenResult.token);
         alert("Token reçu : " + tokenResult.token);
     } catch (error) {
-        console.error("Failed to get token:", error);
         alert("Erreur lors de la récupération du token.");
     }
 };
