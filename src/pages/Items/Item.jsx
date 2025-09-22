66b9d307a6853371f77a70a9174d142f0d1027b1
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-09-22 12:50:40 +0200

    Update Item.jsx

--- src/pages/Items/Item.jsx
+++ src/pages/Items/Item.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import Fleetbo from '../../api/fleetbo'; 
-import { fleetboDB } from '../../config/fleetboConfig';
+import Fleetbo from 'api/fleetbo'; 
+import { fleetboDB } from 'config/fleetboConfig';
+import PageConfig from 'components/common/PageConfig';
 
+
+const ItemHeader = () => {
+    return (
+         <header className='navbar ps-3 pt-3'> 
+            <div> 
+                <button onClick={() => Fleetbo.back()} className="btn-header text-success fs-5 fw-bold">
+                    <i className="fa-solid fa-arrow-left"></i> <span className='ms-3'>Item</span>
+                </button>
+            </div>
+        </header>
+    );
+};
 
 const Item = () => {
     const [itemData, setItemData] = useState(null);
     const [loading, setLoading]   = useState(true);
     const [error, setError]       = useState("");
-
-
+    
     useEffect(() => {
         window.getParam = (id) => {
-            console.log("ID reçu du natif:", id);
             if (id) {
                 Fleetbo.getDoc(fleetboDB, "items", id);
             } else {
@@ -19,25 +30,21 @@ const Item = () => {
                 setLoading(false);
             }
         };
-
         Fleetbo.setDataCallback((response) => {
             if (response.success && response.data) {
                 setItemData(response.data);
             } else {
                 const errorMessage = response.message || "Erreur lors de la récupération du document.";
-                console.error("Erreur de récupération:", errorMessage);
                 setError(errorMessage);
             }
             setLoading(false);
         });
-
         return () => {
             delete window.getParam;
             Fleetbo.setDataCallback(null);
         };
     }, []);
 
-    // Fonction pour afficher le contenu (chargement, erreur, ou données)
     const renderContent = () => {
         if (loading) {
             return (
@@ -46,11 +53,9 @@ const Item = () => {
                 </div>
             );
         }
-
         if (error) {
             return <div className="alert alert-danger">{error}</div>;
         }
-
         if (itemData) {
             return (
                 <div>
@@ -63,13 +68,8 @@ const Item = () => {
     };
     return (
         <>
-            <header className='navbar pt-4'> 
-                <div> 
-                    <button onClick={() => Fleetbo.back()} className="logout fs-5 fw-bold">
-                        <i className="fa-solid fa-arrow-left"></i> <span className='ms-3'>Item</span>
-                    </button>
-                </div>
-            </header>
+            <PageConfig navbar="hidden" />
+            <ItemHeader />
             <div className="p-3">
                 {renderContent()}
             </div>
