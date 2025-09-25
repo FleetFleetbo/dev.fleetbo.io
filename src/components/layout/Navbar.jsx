42e53ab92ec04bef6fa18355f9b270ee13457d7d
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-09-25 19:59:03 +0200

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Fleetbo from 'api/fleetbo';
 import 'assets/css/Navbar.css';
 
