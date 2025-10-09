07ef2832138877d35dc9edbbf99ddc85984d1666
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-10-09 03:55:19 +0200

    Update SetUser.jsx

--- src/pages/Items/User/SetUser.jsx
+++ src/pages/Items/User/SetUser.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Fleetbo from 'api/fleetbo';
 import { fleetboDB } from 'config/fleetboConfig';
 import { motion } from "framer-motion";
 import { useAuth } from 'context/AuthContext';
