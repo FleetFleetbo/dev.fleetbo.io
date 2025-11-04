fc876f7f9ef80be1d8d977780d1057ef61083868
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-11-04 04:15:33 +0100

    Update PageConfig.js

--- src/components/common/PageConfig.js
+++ src/components/common/PageConfig.js
@@ -5,7 +5,7 @@ import React from 'react';
  * This invisible component is used to communicate the page's configuration
  * to the native Fleetbo shell.
  * @param {object} props
- * @param {'visible' | 'hidden'} props.navbar - Defines the navbar's visibility.
+ * @param {'visible' | 'none'} props.navbar - Defines the navbar's visibility.
  */
 const PageConfig = ({ navbar }) => {
     return (
