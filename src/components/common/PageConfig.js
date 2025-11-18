75ee3973bf629a191bd928aa2766e94659b60f64
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-11-18 12:27:33 +0100

    Update PageConfig.js

--- src/components/common/PageConfig.js
+++ src/components/common/PageConfig.js
@@ -1,24 +1,19 @@
-// Dans src/components/common/PageConfig.jsx
+//src/components/common/PageConfig.jsx
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Fleetbo from 'config/systemhelper';
 
 const PageConfig = ({ navbar }) => {
     const location = useLocation();
-
     useEffect(() => {
         const route = location.pathname;
         let navbarState = navbar || 'none';
-
         // Security logic
         if (navbarState !== 'show' && navbarState !== 'visible') {
             navbarState = 'none';
-        }
-        
+        }  
         if (window.Fleetbo && typeof window.Fleetbo.onWebPageReady === 'function') {
             Fleetbo.onWebPageReady(route, navbarState);
         }
-        
     }, [location, navbar]);
     return null;
 };
