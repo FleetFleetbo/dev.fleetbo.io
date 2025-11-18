8a6ae41b0f7bcf8b4ee7b5f5b3e5bee838587d89
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-11-18 12:30:15 +0100

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,13 +4,59 @@ import 'assets/css/Navbar.css';
 
 const navItems = [
   { id: 'Tab1', view: 'tab1', isNative: false, icon: 'fa-solid fa-house' },
-  { id: 'Tab2', view: 'Home', isNative: true,  icon: 'fa-solid fa-crown' }, // Example: { view: 'Home', isNative: true } loads a native component
+  { id: 'Tab2', view: 'tab2', isNative: false,  icon: 'fa-solid fa-crown' }, // Example: { view: 'Home', isNative: true } loads a native component
   { id: 'Tab3', view: 'tab3', isNative: false, icon: 'fa-solid fa-user' },
 ];
 
-const Navbar = () => {
-  const location = useLocation();
-  const [activeTab, setActiveTab] = useState('Tab1');
+const Navbar          = () => {
+  const location      = useLocation();
+  const getInitialTab = () => {
+    // 1. Emulator
+    const params = new URLSearchParams(window.location.search);
+    const activeRoute = params.get('activeRoute');
+    
+    if (activeRoute) {
+      const matchedTab = navItems.find(item => 
+        !item.isNative && activeRoute.includes(`/${item.view}`)
+      );
+      if (matchedTab) {
+        return matchedTab.id;
+      }
+    }
+    
+    // Native
+    const savedTab = localStorage.getItem("activeTab");
+    if (savedTab) {
+      return savedTab;
+    }
+    
+    // 3. Default : Tab1
+    return 'Tab1';
+  };
+
+    useEffect(() => {
+        const handleMessage = (event) => {
+            const { type, route } = event.data;
+            if (type === 'SET_ACTIVE_ROUTE') {
+                const matchedTab = navItems.find(item => 
+                    !item.isNative && route.includes(`/${item.view}`)
+                );
+
+                if (matchedTab) {
+                    setActiveTab(matchedTab.id);
+                }
+            }
+        };
+        window.addEventListener('message', handleMessage);
+        return () => window.removeEventListener('message', handleMessage);
+        
+    }, []);
+
+  const [activeTab, setActiveTab] = useState(getInitialTab);
+  
+  useEffect(() => {
+    localStorage.setItem("activeTab", activeTab);
+  }, [activeTab]);
 
   useEffect(() => {
     const currentTab = navItems.find(item => !item.isNative && location.pathname.includes(item.view));
@@ -21,7 +67,6 @@ const Navbar = () => {
   }, [location]);
 
   const handleSelectTab = (item) => {
-    setActiveTab(item.id);
     Fleetbo.openView(item.view, item.isNative);
   };
 
