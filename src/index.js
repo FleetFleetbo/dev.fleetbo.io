bc7172828041c98f3a87029029ab8dc3922c3736
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-12-14 08:00:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// src/index.js
+import { createBrowserHistory } from "history";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'; 
@@ -6,22 +6,25 @@ import 'assets/css/Index.css';
 import 'assets/css/Native.css'; 
 import App from './App'; 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { history } from 'components/layout/Navigation';
+
+const history = createBrowserHistory(); 
 
 const rootElement = document.getElementById('root');
 
 window.navigateToTab = (route) => {
-  history.push(route);
+  if (history) {
+      history.push(route);
+  } else {
+      window.location.href = route;
+  }
 };
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <>
     <HistoryRouter history={history}>
       <App />
     </HistoryRouter>
-  </>
 );
 
 serviceWorkerRegistration.register();
