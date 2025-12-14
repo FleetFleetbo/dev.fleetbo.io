d2da4278cacedfddc643dbddd0d412b9ab71fbae
FleetFleetbo <tcheuffa4.ja@gmail.com>
2025-12-14 16:32:12 +0100

    Update ProtectedRoute.js

--- src/components/layout/ProtectedRoute.js
+++ src/components/layout/ProtectedRoute.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from 'context/AuthContext';
-import Loader from 'components/common/Loader'; 
 
 const ProtectedRoute                = ({ children }) => {
     const { isLoggedIn, isLoading } = useAuth();
