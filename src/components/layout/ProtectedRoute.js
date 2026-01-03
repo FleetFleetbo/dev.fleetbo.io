6b456ed05cf614b70daf370e33e245119fb3b64a
FleetFleetbo <tcheuffa4.ja@gmail.com>
2026-01-03 07:53:06 +0100

    Update ProtectedRoute.js

--- src/components/layout/ProtectedRoute.js
+++ src/components/layout/ProtectedRoute.js
@@ -3,17 +3,13 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from 'context/AuthContext';
-
 const ProtectedRoute                = ({ children }) => {
     const { isLoggedIn, isLoading } = useAuth();
     const location                  = useLocation();
-
     if (isLoading) { return null; }
     if (!isLoggedIn) {  return <Navigate to="/login" state={{ from: location }} replace />; }
-
     return children;
 };
-
 export default ProtectedRoute;
 
 
