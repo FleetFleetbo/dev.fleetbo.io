d4eb9d7187afb511314fe2ea388cbc7b96adb5aa
FleetFleetbo <tcheuffa4.ja@gmail.com>
2026-01-14 23:00:07 +0100

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Home, Crown, User } from 'lucide-react';
 import 'assets/css/Navbar.css';
 
 
