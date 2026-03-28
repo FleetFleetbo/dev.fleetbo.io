36c812c22a1c1dce8b45e38f8da143bec5b77bec
FleetFleetbo <tcheuffa4.ja@gmail.com>
2026-03-28 21:31:51 +0100

    Update Tab2.jsx

--- src/app/tabs/Tab2.jsx
+++ src/app/tabs/Tab2.jsx
@@ -1,5 +1,5 @@
 /**
- * Native Tab Redirect or Not
+ * Fleetbo Tab Redirect or Not
  *
  * This tab is handled by a native module (or its mock in dev).
  * If React renders this route (e.g. on reload), we redirect to the mock.
@@ -7,18 +7,17 @@
 import { useEffect } from 'react';
 import { PageConfig } from '@fleetbo';
 
-const Tab2 = () => {
-    // Call your fleetbo tab module 
-    useEffect(() => { Fleetbo.openView('SampleTabMock', true);}, []);
-
-    return (
-        <>
-            <PageConfig navbar="show" />
-            <div className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
-                <div className="spinner-border text-success" role="status" />
-            </div>
-        </>
-    );
-};
-
-export default Tab2;
+export default function Tab2() {
+    useEffect(() => {
+           Fleetbo.openView('SampleTab', true, {
+               emit: async (action, payload) => {
+                   if (action === 'PING_JS') {
+                       console.log("Signal reçu du Métal !");
+                       // Vous pouvez ouvrir un overlay ici : await Fleetbo.exec('MonOverlay', 'open');
+                   }
+               }
+           });
+       }, []);
+   
+    return <PageConfig navbar="show" />;
+}
