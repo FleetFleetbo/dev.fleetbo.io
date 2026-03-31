ad50db9e3ea7f466fa41c898d44f02fe7294873c
FleetFleetbo <tcheuffa4.ja@gmail.com>
2026-03-31 23:00:56 +0200

    Update Tab2.jsx

--- src/app/tabs/Tab2.jsx
+++ src/app/tabs/Tab2.jsx
@@ -1,7 +1,7 @@
 /**
  * Fleetbo Tab Redirect or Not
  *
- * This tab is handled by a native module (or its mock in dev).
+ * This tab is handled by a fleetbo module (or its mock in dev).
  * If React renders this route (e.g. on reload), we redirect to the mock.
  */
 import { useEffect, useState } from 'react';
@@ -10,14 +10,16 @@ import { PageConfig } from '@fleetbo';
 export default function Tab2() {
     const [navMode, setNavMode] = useState("show");
     useEffect(() => {
-           Fleetbo.openView('SampleTab', true, {
-               emit: async (action, payload) => {
-                   if (action === 'PING_JS') {
-                       console.log("Signal reçu du Métal !");
-                       // Vous pouvez ouvrir un overlay ici : await Fleetbo.exec('MonOverlay', 'open');
-                   }
-               }
-           });
+        Fleetbo.openView('SampleTab', true, {
+            emit: async (action, payload) => {
+                if (action === 'HIDE_NAVBAR') setNavMode("none");
+                if (action === 'SHOW_NAVBAR') setNavMode("show");
+                if (action === 'PING_JS') {
+                    console.log("Signal reçu du Métal !");
+                    // Example: await Fleetbo.exec('MonOverlay', 'open');
+                }
+            }
+        });
     }, []);
     return (<> <PageConfig navbar={navMode} /> </>);
 }
